e2788a600c1fadc2f7c0ee49e0f6000d20d16765
E.Azer Koçulu <azer@kodfabrik.com>
2016-01-10 20:51:46 -0800

    Add transaction example

---
+++ examples/transaction.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/azer/crud"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+type User struct {
+	Id        int `sql:"auto-increment primary-key"`
+	FirstName string
+	LastName  string
+}
+
+func main() {
+	DB, err := crud.Connect("mysql", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	if err := DB.CreateTables(User{}); err != nil {
+		panic(err)
+	}
+
+	tx, err := DB.Begin()
+
+	if err != nil {
+		panic(err)
+	}
+
+	azer := User{
+		FirstName: "Azer",
+		LastName:  "Koculu",
+	}
+
+	if err := tx.Create(&azer); err != nil {
+		panic(err)
+	}
+
+	tx.Commit()
+
+	copy := User{}
+	if err := DB.Read(&copy, "WHERE first_name='Azer'"); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(copy.Id)
+}
